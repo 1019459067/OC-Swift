9f5d570958f02338b6ab38761a5ec11c8bc357b0
肖伟华 <XWH@XWH-MBP.local>
2016-07-04 23:47:16 +0800

    add "百思不得姐 icon".

---
+++ 百思不得姐/百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by 肖伟华 on 16/7/4.
+//  Copyright © 2016年 XWH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
