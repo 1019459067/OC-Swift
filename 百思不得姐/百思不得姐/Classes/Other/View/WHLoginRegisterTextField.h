a915dc26aa72b033064fd73ed3198a327edc060b
肖伟华 <XWH@XWH-MBP.local>
2016-07-17 16:08:51 +0800

    修改光标文字颜色.

---
+++ 百思不得姐/百思不得姐/Classes/Other/View/WHLoginRegisterTextField.h
@@ -0,0 +1,13 @@
+//
+//  WHLoginRegisterTextField.h
+//  百思不得姐
+//
+//  Created by 肖伟华 on 16/7/17.
+//  Copyright © 2016年 XWH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WHLoginRegisterTextField : UITextField
+
+@end
---
+++ 百思不得姐/百思不得姐/Classes/Other/View/WHLoginRegisterTextField.m
@@ -0,0 +1,18 @@
+//
+//  WHLoginRegisterTextField.m
+//  百思不得姐
+//
+//  Created by 肖伟华 on 16/7/17.
+//  Copyright © 2016年 XWH. All rights reserved.
+//
+
+#import "WHLoginRegisterTextField.h"
+
+@implementation WHLoginRegisterTextField
+
+- (void)awakeFromNib
+{
+    self.tintColor = [UIColor whiteColor];
+}
+
+@end
