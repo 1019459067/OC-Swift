4b9e67c79bba3414e8eb9838b270429bacf2cf7e
xiaoweihua <1019459067@qq.com>
2016-07-21 23:53:46 +0800

    textfield.

--- 百思不得姐/百思不得姐/Classes/Other/View/WHLoginRegisterTextField.m
+++ 百思不得姐/百思不得姐/Classes/Other/View/WHLoginRegisterTextField.m
@@ -13,6 +13,30 @@
 - (void)awakeFromNib
 {
     self.tintColor = [UIColor whiteColor];
+    
+//    NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+//    attributes[NSForegroundColorAttributeName] = [UIColor redColor];
+//    self.attributedPlaceholder = [[NSAttributedString alloc]initWithString:self.placeholder attributes:attributes];
 }
 
+#pragma mark 重写
+//- (CGRect)placeholderRectForBounds:(CGRect)bounds
+//{
+//    return CGRectMake(60, 0, 20, 40);
+//}
+- (void)drawPlaceholderInRect:(CGRect)rect
+{
+    NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+    attributes[NSForegroundColorAttributeName] = [UIColor whiteColor];
+    attributes[NSFontAttributeName] = self.font;
+
+//    CGRect rectPlaceholder ;
+//    rectPlaceholder.size.width =  rect.size.width;
+//    rectPlaceholder.size.width =  self.font.lineHeight;
+//    rectPlaceholder.origin.x = 0;
+//    rectPlaceholder.origin.y = (rect.size.height - rectPlaceholder.size.width)/2.0;
+//    [self.placeholder drawInRect:CGRectMake(0, 10, rect.size.width, rect.size.height) withAttributes:attributes];
+    
+    [self.placeholder drawAtPoint:CGPointMake(0, (rect.size.height - self.font.lineHeight)/2.0) withAttributes:attributes];
+}
 @end
