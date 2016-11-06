6288c30c0371e63e0e6280407f9e3eda72050c59
1019459067 <XWH@XWH-MBP.local>
2016-11-06 14:13:45 +0800

    init core annimation demo.

---
+++ Core Animation Demo/Core Animation Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core Animation Demo
+//
+//  Created by 肖伟华 on 2016/11/5.
+//  Copyright © 2016年 XWH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Core Animation Demo/Core Animation Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core Animation Demo
+//
+//  Created by 肖伟华 on 2016/11/5.
+//  Copyright © 2016年 XWH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
