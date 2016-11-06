beb168cae4ad9f42f7afcaebd879a801cb0e4748
1019459067 <XWH@XWH-MBP.local>
2016-11-06 17:27:41 +0800

    关键帧动画/贝塞尔曲线.

--- Core Animation Demo/Core Animation Demo/Test5ViewController.m
+++ Core Animation Demo/Core Animation Demo/Test5ViewController.m
@@ -29,22 +29,23 @@
  
     [self translationAnimation];
 }
-#pragma mark - 关键帧动画
+#pragma mark - 关键帧动画/贝塞尔曲线
 - (void)translationAnimation
 {
     //1.创建关键帧动画并设置动画属性
     CAKeyframeAnimation *keyframeAnimation=[CAKeyframeAnimation animationWithKeyPath:@"position"];
     
     //2.设置关键帧,这里有四个关键帧
-    NSValue *key1=[NSValue valueWithCGPoint:_layer.position];//对于关键帧动画初始值不能省略
-    NSValue *key2=[NSValue valueWithCGPoint:CGPointMake(80, 220)];
-    NSValue *key3=[NSValue valueWithCGPoint:CGPointMake(45, 300)];
-    NSValue *key4=[NSValue valueWithCGPoint:CGPointMake(55, 400)];
-    NSArray *values=@[key1,key2,key3,key4];
-    keyframeAnimation.values=values;
+    //绘制贝塞尔曲线
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, self.layer.position.x, self.layer.position.y);//移动到起始点
+    CGPathAddCurveToPoint(path, NULL, 160, 280, -30, 300, 55, 400);//绘制二次贝塞尔曲线
+    
+    keyframeAnimation.path=path;//设置path属性
+    CGPathRelease(path);//释放路径对象
     //设置其他属性
     keyframeAnimation.duration=8.0;
-    keyframeAnimation.beginTime=CACurrentMediaTime()+2;//设置延迟2秒执行
+    keyframeAnimation.beginTime=CACurrentMediaTime()+5;//设置延迟2秒执行
     
     
     //3.添加动画到图层，添加动画后就会执行动画
