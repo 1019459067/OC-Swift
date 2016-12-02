73cf27136cbcf293cb716bdd90f33dde35434bf4
xiaoweihua <xiaoweihua@sensetime.com>
2016-12-02 14:27:10 +0800

    Wet Koala Demo.

---
+++ Wet Koala Demo/Wet Koala Demo/Class/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainViewController.m
+//  Wet Koala Demo
+//
+//  Created by STMBP on 2016/12/2.
+//  Copyright © 2016年 1019459067. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Wet Koala Demo/Wet Koala Demo/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wet Koala Demo
+//
+//  Created by STMBP on 2016/12/2.
+//  Copyright © 2016年 1019459067. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
