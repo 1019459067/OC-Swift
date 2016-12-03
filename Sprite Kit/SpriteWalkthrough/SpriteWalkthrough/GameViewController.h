a4723b082ebab6d1a6dd9644c59c06ad7b148420
xiaoweihua <xiaoweihua@sensetime.com>
2016-12-03 20:40:33 +0800

    init SpriteWalkthrough.

---
+++ Sprite Kit/SpriteWalkthrough/SpriteWalkthrough/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  SpriteWalkthrough
+//
+//  Created by STMBP on 2016/12/3.
+//  Copyright © 2016年 1019459067. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ Sprite Kit/SpriteWalkthrough/SpriteWalkthrough/GameViewController.m
@@ -0,0 +1,44 @@
+//
+//  GameViewController.m
+//  SpriteWalkthrough
+//
+//  Created by STMBP on 2016/12/3.
+//  Copyright © 2016年 1019459067. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    SKView *skView = (SKView *)self.view;
+
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    skView.showsDrawCount = YES;
+}
+
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+@end
