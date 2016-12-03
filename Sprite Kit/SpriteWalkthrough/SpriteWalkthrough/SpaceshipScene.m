a36e35de927a37b4d1d171e93b956fc4f55498b6
1019459067 <XWH@XWH-MBP.local>
2016-12-04 00:06:11 +0800

    添加飞船.

--- Sprite Kit/SpriteWalkthrough/SpriteWalkthrough/SpaceshipScene.m
+++ Sprite Kit/SpriteWalkthrough/SpriteWalkthrough/SpaceshipScene.m
@@ -25,5 +25,48 @@
 {
     self.backgroundColor = [SKColor blackColor];
     self.scaleMode = SKSceneScaleModeAspectFit;
+    
+    SKSpriteNode *spaceShip = [self newSpaceShip];
+    spaceShip.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    [self addChild:spaceShip];
+    
+}
+- (SKSpriteNode *)newSpaceShip
+{
+    SKSpriteNode *hull = [[SKSpriteNode alloc]initWithColor:[SKColor grayColor] size:CGSizeMake(33, 24)];
+    SKAction *hover = [SKAction sequence:@[[SKAction waitForDuration:1],
+                                           [SKAction moveByX:100 y:50 duration:1],
+                                           
+                                           [SKAction waitForDuration:1.0],
+                                           [SKAction moveByX:-100 y:-50 duration:1],
+                                           
+                                           [SKAction waitForDuration:1.0],
+                                           [SKAction moveByX:-100 y:50 duration:1],
+
+                                           [SKAction waitForDuration:1.0],
+                                           [SKAction moveByX:100 y:-50 duration:1],
+                                           ]];
+    [hull runAction:[SKAction repeatActionForever:hover] completion:^{
+        
+    }];
+    
+    SKSpriteNode *light1 = [self newlight];
+    light1.position = CGPointMake(-28, 6);
+    [hull addChild:light1];
+    
+    SKSpriteNode *light2 = [self newlight];
+    light2.position = CGPointMake(28, 6);
+    [hull addChild:light2];
+    return hull;
+}
+
+- (SKSpriteNode *)newlight
+{
+    SKSpriteNode *light = [[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(8, 8)];
+    SKAction *blink = [SKAction sequence:@[[SKAction fadeOutWithDuration:0.25],
+                                           [SKAction fadeInWithDuration:0.25]
+                                           ]];
+    [light runAction:[SKAction repeatActionForever:blink]];
+    return light;
 }
 @end
