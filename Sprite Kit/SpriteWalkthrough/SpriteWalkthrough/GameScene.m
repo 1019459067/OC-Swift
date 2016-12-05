e3e97fb65394e2a98642aa644629632d61c91186
xiaoweihua <xiaoweihua@sensetime.com>
2016-12-05 16:02:30 +0800

    add rocks drop.

--- Sprite Kit/SpriteWalkthrough/SpriteWalkthrough/GameScene.m
+++ Sprite Kit/SpriteWalkthrough/SpriteWalkthrough/GameScene.m
@@ -35,11 +35,35 @@
     [self addChild:back];
 
     [self newShip];
+
+        /// add rocks
+    SKAction *makeRocks = [SKAction sequence:@[[SKAction performSelector:@selector(addRocks) onTarget:self],
+                                               [SKAction waitForDuration:0.1 withRange:0.01]]];
+    [self runAction:[SKAction repeatActionForever:makeRocks]];
+}
+static inline CGFloat skRandf()
+{
+    return rand()/(CGFloat)RAND_MAX;
+}
+static inline CGFloat skRand(CGFloat start,CGFloat end)
+{
+    return skRandf()*(end-start)+start;
+}
+- (void)addRocks
+{
+    SKSpriteNode *rock = [[SKSpriteNode alloc]initWithColor:[SKColor brownColor] size:CGSizeMake(8, 8)];
+    rock.name = @"rock";
+    rock.physicsBody.usesPreciseCollisionDetection = YES;
+    rock.position = CGPointMake(skRand(0, self.size.height), self.size.height);
+    rock.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rock.size];
+    [self addChild:rock];
 }
 - (void)newShip
 {
     SKSpriteNode *hull = [[SKSpriteNode alloc]initWithColor:[SKColor grayColor] size:CGSizeMake(30, 20)];
     hull.name = @"ship";
+    hull.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:hull.size];
+    hull.physicsBody.dynamic = NO;
     hull.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     [self addChild:hull];
 
@@ -75,6 +99,9 @@
 {
     SKSpriteNode *light = [[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(2, 2)];
     light.position = point;
+    light.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:light.size];
+    light.physicsBody.dynamic = NO;
+
         /// add action
     SKAction *fadeIn = [SKAction fadeInWithDuration:0.38];
     SKAction *fadeOut = [SKAction fadeOutWithDuration:0.38];
@@ -96,5 +123,14 @@
     CGPoint pointTouch = [[touches anyObject]locationInView:self.view];
     NSLog(@"%@",NSStringFromCGPoint(pointTouch));
     node.position = CGPointMake(pointTouch.x, self.size.height-pointTouch.y);
+}
+- (void)didSimulatePhysics
+{
+    [self enumerateChildNodesWithName:@"rock" usingBlock:^(SKNode * _Nonnull node, BOOL * _Nonnull stop) {
+        if (node.position.y<0)
+        {
+            [node removeFromParent];
+        }
+    }];
 }
 @end
