20d4d91d00b7d491d8c966637770da06d10aa612
1019459067 <XWH@XWH-MBP.local>
2017-02-05 21:51:21 +0800

    OpenGL_ES_02.

--- OpenGL_ES3.0/书本/OpenGL_ES_02/OpenGL_ES_02/ViewController.m
+++ OpenGL_ES3.0/书本/OpenGL_ES_02/OpenGL_ES_02/ViewController.m
@@ -7,16 +7,63 @@
 //
 
 #import "ViewController.h"
+#import <OpenGLES/ES3/gl.h>
+#import <OpenGLES/ES3/glext.h>
+
+typedef struct {
+    GLKVector3  positionCoords;
+}
+SceneVertex;
+
+static const SceneVertex vertices[] =
+{
+    {{-0.5f, -0.5f, 0.0}}, // lower left corner
+    {{ 0.5f, -0.5f, 0.0}}, // lower right corner
+    {{-0.5f,  0.5f, 0.0}}  // upper left corner
+};
 
 @interface ViewController ()
-
+{
+    GLuint vertexBufferID;
+}
+@property (strong, nonatomic) GLKBaseEffect *baseEffect;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    GLKView *glkView = (GLKView *)self.view;
+    NSAssert([glkView isKindOfClass:[GLKView class]],@"View controller's view is not a GLKView");
+    glkView.context = [[EAGLContext alloc]initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [EAGLContext setCurrentContext:glkView.context];
+    
+    self.baseEffect = [[GLKBaseEffect alloc]init];
+    self.baseEffect.useConstantColor = GL_TRUE;
+    self.baseEffect.constantColor = GLKVector4Make(1, 1, 1, 1);
+    glClearColor(0, 0, 0, 1);
+    
+    glGenBuffers(1, &vertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+}
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
+{
+    [self.baseEffect prepareToDraw];
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+- (void)dealloc
+{
+    [EAGLContext setCurrentContext:nil];
 }
 
 - (void)didReceiveMemoryWarning {
