5392ed50305c374f88ba42767f87f45c619288f3
xiaoweihua <xiaoweihua@sensetime.com>
2017-02-09 09:09:08 +0800

    OpenGL_ES_08.

---
+++ OpenGL_ES3.0/书本/OpenGL_ES_08/OpenGL_ES_08/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenGL_ES_08
+//
+//  Created by STMBP on 2017/2/8.
+//  Copyright © 2017年 sensetime. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
---
+++ OpenGL_ES3.0/书本/OpenGL_ES_08/OpenGL_ES_08/ViewController.m
@@ -0,0 +1,79 @@
+//
+//  ViewController.m
+//  OpenGL_ES_08
+//
+//  Created by STMBP on 2017/2/8.
+//  Copyright © 2017年 sensetime. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AGLKContext.h"
+#import "AGLKVertexAttribArrayBuffer.h"
+
+typedef struct {
+    GLKVector3  positionCoords;
+    GLKVector2  textureCoords;
+}
+SceneVertex;
+
+static const SceneVertex vertices[] =
+{
+    {{-1.0f, -0.67f, 0.0f}, {0.0f, 0.0f}},  // first triangle
+    {{ 1.0f, -0.67f, 0.0f}, {1.0f, 0.0f}},
+    {{-1.0f,  0.67f, 0.0f}, {0.0f, 1.0f}},
+    {{ 1.0f, -0.67f, 0.0f}, {1.0f, 0.0f}},  // second triangle
+    {{-1.0f,  0.67f, 0.0f}, {0.0f, 1.0f}},
+    {{ 1.0f,  0.67f, 0.0f}, {1.0f, 1.0f}},
+};
+
+@interface ViewController ()
+@property (strong, nonatomic) GLKBaseEffect *baseEffect;
+@property (strong, nonatomic) AGLKVertexAttribArrayBuffer *vertexBuffer;
+@property (strong, nonatomic) GLKTextureInfo *textureInfo0;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    GLKView *glkView = (GLKView *)self.view;
+    glkView.context = [[AGLKContext alloc]initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [AGLKContext setCurrentContext:glkView.context];
+
+    self.baseEffect = [[GLKBaseEffect alloc]init];
+    self.baseEffect.useConstantColor = GL_TRUE;
+    self.baseEffect.constantColor = GLKVector4Make(1, 1, 1, 1);
+    ((AGLKContext *)glkView.context).clearColor = GLKVector4Make(0, 0, 0, 1);
+
+    self.vertexBuffer = [[AGLKVertexAttribArrayBuffer alloc]initWithAttribStride:sizeof(SceneVertex) numberOfVertices:sizeof(vertices)/sizeof(SceneVertex) data:vertices usage:GL_TRIANGLES];
+
+    CGImageRef imageRef0 = [UIImage imageNamed:@"leaves.gif"].CGImage;
+    CGImageRef imageRef1 = [UIImage imageNamed:@"beetle"].CGImage;
+
+    GLKTextureInfo *textureInfo0 = [GLKTextureLoader textureWithCGImage:imageRef0 options:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES],GLKTextureLoaderOriginBottomLeft, nil] error:NULL];
+    self.baseEffect.texture2d0.name = textureInfo0.name;
+    self.baseEffect.texture2d0.target = textureInfo0.target;
+
+    GLKTextureInfo *textureInfo1 = [GLKTextureLoader textureWithCGImage:imageRef1 options:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES],GLKTextureLoaderOriginBottomLeft, nil] error:NULL];
+    self.baseEffect.texture2d1.name = textureInfo1.name;
+    self.baseEffect.texture2d1.target = textureInfo1.target;
+    self.baseEffect.texture2d1.envMode = GLKTextureEnvModeDecal;
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
+{
+    [(AGLKContext *)view.context clear:GL_COLOR_BUFFER_BIT];
+
+    [self.vertexBuffer prepareToDrawWithAttrib:GLKVertexAttribPosition numberOfCoordinates:3 attribOffset:offsetof(SceneVertex, positionCoords) shouldEnable:YES];
+    [self.vertexBuffer prepareToDrawWithAttrib:GLKVertexAttribTexCoord0 numberOfCoordinates:2 attribOffset:offsetof(SceneVertex, textureCoords) shouldEnable:YES];
+    [self.vertexBuffer prepareToDrawWithAttrib:GLKVertexAttribTexCoord1 numberOfCoordinates:2 attribOffset:offsetof(SceneVertex, textureCoords) shouldEnable:YES];
+
+    [self.baseEffect prepareToDraw];
+
+    [self.vertexBuffer drawArrayWithMode:GL_TRIANGLES startVertexIndex:0 numberOfVertices:sizeof(vertices)/sizeof(SceneVertex)];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
