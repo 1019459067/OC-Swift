4ec9d171fff148d6425d6b694cac9909ce85de8a
xiaoweihua <xiaoweihua@sensetime.com>
2018-05-21 16:48:06 +0800

    replace key word

--- Block's strong&weak/Block's strong&weak/Car.m
+++ Block's strong&weak/Block's strong&weak/Car.m
@@ -3,7 +3,7 @@
 //  Block's strong&weak
 //
 //  Created by STMBP on 16/8/22.
-//  Copyright © 2016年 sensetime. All rights reserved.
+//  Copyright © 2016年 1019459067. All rights reserved.
 //
 
 #import "Car.h"
